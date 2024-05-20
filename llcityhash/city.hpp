3b47c0ec920733dd4fcfff7bc670899b805b274d
Llanyro <Llanyro@gmail.com>
2024-05-20 21:32:02 +0200

    Update city and submodule

--- llcityhash/city.hpp
+++ llcityhash/city.hpp
@@ -70,7 +70,7 @@
 #ifndef LLCPP_CITY_HASH_HPP_
 #define LLCPP_CITY_HASH_HPP_
 
-#include <llanylib/types.hpp>
+#include <llanylib/traits.hpp>
 
 #include <utility>
 
@@ -78,8 +78,8 @@ namespace city {
 
 using hash128 = std::pair<ui64, ui64>;
 
-inline ui64 Uint128Low64(const hash128& x) { return x.first; }
-inline ui64 Uint128High64(const hash128& x) { return x.second; }
+__LL_NODISCARD__ __LL_INLINE__ ui64 Uint128Low64(const hash128& x) { return x.first; }
+__LL_NODISCARD__ __LL_INLINE__ ui64 Uint128High64(const hash128& x) { return x.second; }
 
 // Hash function for a byte array.
 LL_SHARED_LIB ui64 CityHash64(ll_string_t buf, len_t len);
@@ -92,6 +92,15 @@ LL_SHARED_LIB ui64 CityHash64WithSeed(ll_string_t buf, const len_t len, const ui
 // hashed into the result.
 LL_SHARED_LIB ui64 CityHash64WithSeeds(ll_string_t buf, const len_t len, const ui64 seed0, const ui64 seed1);
 
+template<class T, class tmp = llcpp::traits::template_types<T>, class W = typename tmp::cinput>
+__LL_NODISCARD__ __LL_INLINE__ ui64 CityHash64(W data) __LL_EXCEPT__ {
+	return city::CityHash64(&data, sizeof(T));
+}
+template<class T, len_t N>
+__LL_NODISCARD__ __LL_INLINE__ ui64 CityHash64(const T(&data)[N]) __LL_EXCEPT__ {
+	return city::CityHash64(data, sizeof(T) * N);
+}
+
 // Proxy linkage for C
 LL_SHARED_LIB void CityHash128(ll_string_t s, len_t len, hash128& result);
 
@@ -103,7 +112,7 @@ LL_SHARED_LIB ui32 CityHash32(ll_string_t buf, len_t len);
 
 // Hash 128 input bits down to 64 bits of output.
 // This is intended to be a reasonably good hash function.
-LL_SHARED_LIB inline ui64 Hash128to64(const hash128& x) {
+LL_SHARED_LIB __LL_INLINE__ ui64 Hash128to64(const hash128& x) {
   // Murmur-inspired hashing.
   const ui64 kMul = 0x9ddfea08eb382d69ULL;
   ui64 a = (Uint128Low64(x) ^ Uint128High64(x)) * kMul;
